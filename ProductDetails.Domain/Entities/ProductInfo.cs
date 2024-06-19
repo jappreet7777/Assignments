85cc8857bb68d44b05ad02edb2b2ebee993e12bd
jappreet7777 <jappreet.singh@nagarro.com>
2024-06-19 11:30:46 +0530

    API | Product Details Service

---
+++ ProductDetails.Domain/Entities/ProductInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDetails.Domain.Entities
+{
+    public class ProductInfo
+    {
+        [Key]
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string  Design {  get; set; }
+        public decimal Quantity { get; set; }
+
+        public ProductInfo()
+        {
+           Id = Guid.NewGuid().ToString();
+           CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ ProductDetails.Infrastructure/AppDBcontext/ProductDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductDetails.Domain.Entities;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDetails.Infrastructure.ProductDetails
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductInfo> ProductInfo { get; set; }
+
+       
+    }
+}
